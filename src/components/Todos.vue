d7133058e99153b4fc6e5905a7a80fec4d1ccbdf
Ninette Adhikari <ninette.adhikari@gmail.com>
2020-05-13 19:21:20 +0200

    feat: Add TodoItem

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -8,17 +8,27 @@
         class="todo"
       >
         {{todo.title}}
+        <i
+          @click='deleteTodo(todo.id)'
+          class='fas fa-trash-alt'
+        ></i>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import { mapGetters, mapActions } from 'vuex'
 
 export default {
   name: 'Todos',
-  computed: mapGetters(['allTodos'])
+  methods: {
+    ...mapActions(['fetchTodos', 'deleteTodo'])
+  },
+  computed: mapGetters(['allTodos']),
+  created () {
+    this.fetchTodos()
+  }
 }
 </script>
 
